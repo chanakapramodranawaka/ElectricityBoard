7a160b5c6f789f68d8567b547133977e59ee6856
Chanaka Pramod <67319115+chanakapramodranawaka@users.noreply.github.com>
2023-10-02 12:03:17 +0530

    1v

---
+++ mnthlyBill.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int bill_amount,total,count;
+
+    for(count=1; count<=10; count ++)
+    {
+        printf("Please enter %d bill amount \n",count);
+        scanf("%d",&bill_amount);
+
+        total = bill_amount + total;
+    }
+
+    printf("Total is : %d", total);
+
+    return 0;
+}
